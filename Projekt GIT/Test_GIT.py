8274e9ac1ff889e6a9a584f40a74efeff7743e50
emamaj <edyta_m@hotmail.com>
2020-06-03 22:12:12 +0200

    Add message print

---
+++ Projekt GIT/Test_GIT.py
@@ -0,0 +1,11 @@
+print("Testing GIT")
+print("PyCharm 2020.1.1 (Community Edition)
+Build #PC-201.7223.92, built on April 30, 2020
+Runtime version: 11.0.6+8-b765.40 amd64
+VM: OpenJDK 64-Bit Server VM by JetBrains s.r.o
+Linux 4.15.0-101-generic
+GC: ParNew, ConcurrentMarkSweep
+Memory: 951M
+Cores: 4
+
+Current Desktop: ubuntu:GNOME")
