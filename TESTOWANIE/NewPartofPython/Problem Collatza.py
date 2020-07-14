ae1d60bcb1da283adde1b31043e39c3fb5ba7ff3
emamaj <edyta.m.info@gmail>
2020-07-14 20:46:39 +0100

    problem resolve

---
+++ TESTOWANIE/NewPartofPython/Problem Collatza.py
@@ -0,0 +1,23 @@
+#Problem Collatza
+
+
+
+def collatz(number):
+    if number % 2 == 0:
+        number = number // 2
+        print(number)
+        return number
+    elif number % 2 == 1:
+         number = number * 3 + 1
+         print(number)
+         return number
+
+print('Podaj liczbę')
+try:
+    liczba = int(input())
+    while True:
+        liczba = collatz(liczba)
+        if liczba == 1:
+            break
+except ValueError:
+    print('Musisz podać liczbę całkowitą')
